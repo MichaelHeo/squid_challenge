c3cf19241d3035e85df24119f50a416cb2bec5cc
Michael Heo <michael.heo@hashlab.com>
2018-01-26 18:44:23 -0200

    port changed to process.env.PORT

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = require('./config/custom-express')() // Chama o module.exports do custom-expresso
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Conectado na porta 3000')
 })
